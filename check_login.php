7f7fa6f03a901f23d7b82c8aafdb1d84f530ca80
Manichaya Sommang <155959135+Manichaya@users.noreply.github.com>
2024-04-02 11:46:46 +0700

    LogIn

--- check_login.php
+++ check_login.php
@@ -12,22 +12,7 @@
     if($count_m>0){
 
         $_SESSION['check_log_1']=$row_m['username'];
-        // $_SESSION['check_log_2']=$row_m['password'];
-        // $_SESSION['check_log_3']=$row_m['status'];
-
-        // if($row_m['status']=="1"){
-        //     echo "<script>";
-        //     echo "alert('ยินดีต้อนรับ admin เข้าสู่ระบบ');";
-        //     echo "window.location.href='admin/'";
-        //     echo "</script>";             
-        // }else{
-        //     echo "<script>";
-        //     echo "alert('ยินดีต้อนรับ user เข้าสู่ระบบ');";
-        //     echo "window.location.href='user/'";
-        //     echo "</script>";               
-        // }
-
-
+        
         echo "<script>";
         echo "window.location.href='home.php'";
         echo "</script>";       
